18cac79e8b60d294230a64495d901524f1618589
Young <yuyazhang7@gmail.com>
2021-10-29 16:47:46 +0800

    Delete useless bricks

--- GameRepo/src/testCase/TestDrawingScene.java
+++ GameRepo/src/testCase/TestDrawingScene.java
@@ -19,27 +19,12 @@ public class TestDrawingScene extends Scene{
 
 	Mouse mouse;
 	Key key;
-	Texture testTexture;
 	BufferedImage x;
 	Map m;
 	BoxController box;
 	Dio dio;
 	@Override
 	public void enter() {
-		
-		InputStream stream = ResourceLoader.load(TestDrawingScene.class, "res/textures/bricks.jpg", "/textures/bricks.jpg" );
-		
-		BufferedImage image = null;
-		try {
-			image = ImageIO.read(stream);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		Vector2f position = new Vector2f(GlobalConstants.WORLD_WIDTH / 2.0f, GlobalConstants.WORLD_HEIGHT / 2.0f);
-		Vector2f scale = new Vector2f(1.0f, 1.0f);
-		Transform transform = new Transform(position, scale);
-		Texture texture = new Texture(image, transform);
-		testTexture = texture;
 		
 		m = Map.getInstance();
 		m.initialize(10, 10, 200, 200, "./res/word.txt"); 
@@ -65,8 +50,7 @@ public class TestDrawingScene extends Scene{
 
 	@Override
 	public void render(Graphics2D g) {
-		AffineTransform transform = AffineTransform.getTranslateInstance(400, 300);
-		g.drawImage(testTexture.getImage(), transform, null);
+
 		m.render(g);
 		box.render(g);
 		dio.render(g);
