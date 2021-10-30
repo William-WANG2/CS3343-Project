bd96218f8a11ea1e455a4295b5c2bd341375a79b
Ruixin-LI <ruixinli7-c@my.cityu.edu.hk>
2021-10-30 10:10:50 +0800

    reader

--- GameRepo/src/fileReader/XMLReader.java
+++ GameRepo/src/fileReader/XMLReader.java
@@ -1,16 +1,18 @@
 package fileReader;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+
 
 import exception.ExMapExceedWordSize;
 import keyValue.*;
@@ -25,36 +27,44 @@ public class XMLReader{
 	 **/
 	public static ArrayList<Info> convert(String path, int m, int n) throws ExMapExceedWordSize {
 		ArrayList<Info> wordlist = new ArrayList<Info>();
+		ArrayList<Info> res = new ArrayList<Info>();
 		NodeList wl = getFile(path);
-		if (m * n > wl.getLength() + 1) {
+		for(int i=0;i<wl.getLength();i++) {
+			Node node = wl.item(i);
+			if(node.getNodeType()==Node.ELEMENT_NODE) {
+				NodeList cl = node.getChildNodes();
+				String def = "";
+				String ans = "";
+				for(int j=0;j<cl.getLength();j++) {
+					Node d=cl.item(j);
+					if(d.getNodeType()==Node.ELEMENT_NODE && d.getNodeName()=="define") {
+						def= d.getTextContent();
+					}
+					if(d.getNodeType()==Node.ELEMENT_NODE && d.getNodeName()=="answer") {
+						ans= d.getTextContent();
+					}
+				}
+				Info word = new WordInfo(def, ans);
+				wordlist.add(word);
+			}
+		}
+		if (m * n > wordlist.size() + 1) {
 			throw new ExMapExceedWordSize();
 		}
 		int startPosInList = new Random().nextInt(wl.getLength() + 1 - m * n);
 		for (int i = startPosInList; i < startPosInList + m * n; i++) {
-			Node w = wl.item(i);
-			String d = "";
-			String a = "";
-			NodeList dl = w.getChildNodes();
-			for (int j = 0; j < dl.getLength(); j++) {
-				if (dl.item(j).getNodeName() == "define") {
-					d = dl.item(j).getNodeValue();
-				} else if (dl.item(j).getNodeName() == "answer") {
-					a = dl.item(j).getNodeValue();
-				}
-			}
-			WordInfo word = new WordInfo(d, a);
-			wordlist.add(word);
+			res.add(wordlist.get(i));
 		}
-		return wordlist;
+		return res;
 	}
 	
 	public static NodeList getFile(String path) {
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		DocumentBuilder db;
 		try {
-			db = dbf.newDocumentBuilder();
+			DocumentBuilder db = dbf.newDocumentBuilder();
 			Document document = db.parse(path);
-			return document.getChildNodes();//getElementsByTagName("word");
+			NodeList nl= document.getElementsByTagName("word");
+			return nl;
 		} catch (ParserConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- GameRepo/src/junitTest/fileReaderTest.java
+++ GameRepo/src/junitTest/fileReaderTest.java
@@ -2,10 +2,12 @@ package junitTest;
 
 import static org.junit.Assert.assertEquals;
 
+
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 import org.junit.Test;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import exception.ExMapExceedWordSize;
@@ -24,10 +26,31 @@ public class fileReaderTest {
 	@Test
 	public void test1() throws ExMapExceedWordSize {
 		String p = "./res/word.xml";
-		NodeList res = XMLReader.getFile(p);
-		
-		assertEquals(null,res);
-		
+		NodeList l = XMLReader.getFile(p);
+		for(int i=0;i<l.getLength();i++) {
+			Node n = l.item(i);
+			if(n.getNodeType()==Node.ELEMENT_NODE) {
+				NodeList da = n.getChildNodes();
+				for(int j=0;j<da.getLength();j++) {
+					Node d=da.item(j);
+					int k=0;
+					if(d.getNodeType()==Node.ELEMENT_NODE && d.getNodeName()=="answer") {
+						String def= d.getTextContent();
+						k=0;
+					}
+				}
+				int j=1;
+			}
+		}
+		//String res=n.getChildNodes().item(0).getNodeValue();
+		//assertEquals(true, "a".equals(res));
+	}
+	@Test
+	public void test10() throws ExMapExceedWordSize, FileNotFoundException {
+		String p = "./res/word.txt";
+		ArrayList<Info> l = TxtReader.convert(p, 0, 0);
+		String res=l.get(0).getAns();
+		assertEquals("a", res);
 	}
 	@Test
 	public void test2() throws ExMapExceedWordSize {
