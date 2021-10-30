bc72eaf49e35836e531bd8f877420eb5f7b0ffd9
Young <yuyazhang7@gmail.com>
2021-10-30 14:35:17 +0800

    Add interface of ResultScene

---
+++ GameRepo/src/scenes/ResultScene.java
@@ -0,0 +1,31 @@
+package scenes;
+
+import java.awt.Graphics2D;
+
+public class ResultScene extends Scene{
+
+	@Override
+	public void enter() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
