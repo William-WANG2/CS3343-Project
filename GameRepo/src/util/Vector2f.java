01ca5a79e373b24971bb30b9d17f082ac714451b
konolmyda <mingyaliu8@my.cityu.edu.hk>
2021-11-21 22:50:53 +0800

    sdgsgsgdsd

--- GameRepo/src/util/Vector2f.java
+++ GameRepo/src/util/Vector2f.java
@@ -7,41 +7,13 @@ public class Vector2f {
 
 	
 
-
+	
 
 	public Vector2f(float x, float y) {
 		this.x = x;
 		this.y = y;
 		this.w = 1.0f;
 	}
-
-
-
-	
-
-	
-
-	
-
-
-
-
-	
-
-
-	
-
-
-	
-
-	
-
-
-
-	
-
-	
-
 
 	
 }
