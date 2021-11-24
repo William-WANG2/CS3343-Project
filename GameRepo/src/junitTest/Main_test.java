aca8bc1b809a2f8fb3fa045955e55207cd2ec7b2
Ruixin-LI <ruixinli7-c@my.cityu.edu.hk>
2021-11-24 17:25:19 +0800

    game application test

--- GameRepo/src/junitTest/Main_test.java
+++ GameRepo/src/junitTest/Main_test.java
@@ -2,12 +2,22 @@ package junitTest;
 
 import static org.junit.Assert.assertEquals;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 import org.junit.Test;
 
 import game.EntryPoint;
 import game.GREGame;
 import gameObject.EnumVocabularyBook;
 import gameObject.Map;
+import scenes.LoginScene;
 import scenes.ResultScene;
 import util.GameApplication;
 import util.GameTimer;
@@ -48,13 +58,56 @@ public class Main_test {
 		game.initialize();
 		ResultScene r = new ResultScene();
 		game.loadScene(r);
-
+	}
+	//GameApplication.run()
+	@Test
+	public void test05() throws AWTException, InterruptedException {
+		GameApplication game = new GREGame(); 
+		game.initialize();
+		game.loadScene(new ResultScene());
+		SwingUtilities.invokeLater(new Runnable() {
+			//Start running the game instance.
+			public void run(){}
+		});	
+		Thread.sleep(10);
+		Thread.currentThread().interrupt();
+	}
+	@Test
+	public void test06() throws AWTException {
+		GameApplication game = new GREGame(); 
+		game.initialize();
+		game.loadScene(new ResultScene());
+		SwingUtilities.invokeLater(new Runnable() {
+			//Start running the game instance.
+			public void run() {Thread.currentThread().interrupt();} 
+		});	
+	}
+	//GameApplication.initialize()
+	@Test
+	public void test0000() throws AWTException {
+		GREGame game = new GREGame();
+		game.initialize();
+		Robot bot = new Robot();
+		int mask = InputEvent.BUTTON1_DOWN_MASK;
+		bot.mouseMove(100, 100);
+		bot.mousePress(mask);
+		bot.mouseRelease(mask);
+	}
+	
+	@Test
+	public void test000() throws AWTException {
+		GREGame game = new GREGame();
+		game.initialize();
+		Robot rb = new Robot();
+		rb.keyPress(KeyEvent.VK_ENTER);
+		rb.keyRelease(KeyEvent.VK_ENTER);
+		assertEquals(game.key.queuingEvent.get(0).intValue(),KeyEvent.VK_ENTER);
 	}
 
 	// System Testing
 	// main
 	@Test
-	public void test05() {
+	public void test000000() {
 		EntryPoint e = new EntryPoint();
 		EntryPoint.main(null);
 	}
